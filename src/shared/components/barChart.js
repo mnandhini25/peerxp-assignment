b8ba687f822dfdb51161f4fc4d0c3cd7a96ce75f
mnandy25 <mnandy25@gmail.com>
2021-09-16 23:05:03 +0530

    removed code

--- src/shared/components/barChart.js
+++ src/shared/components/barChart.js
@@ -10,15 +10,6 @@ import {
 } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from '@devexpress/dx-react-chart';
 
-const data = [
-  { year: '1950', population: 2.525 },
-  { year: '1960', population: 3.018 },
-  { year: '1970', population: 3.682 },
-  { year: '1980', population: 4.440 },
-  { year: '1990', population: 5.310 },
-  { year: '2000', population: 6.127 },
-  { year: '2010', population: 6.930 },
-];
 
 const  BarChartComponent = (props) => {
     console.log('chart props:', props)
